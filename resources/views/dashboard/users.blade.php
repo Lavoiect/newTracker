7bf43c87e3f99b90278defc9895934d255a33ade
Chris Lavoie <lavoiect@gmail.com>
2020-06-12 16:30:52 -0600

    fixed user page

--- resources/views/dashboard/users.blade.php
+++ resources/views/dashboard/users.blade.php
@@ -20,44 +20,45 @@
          <h1>Users</h1>
 
          <div class="row">
-             @foreach ($users as $user)
-             <div class="col-3">
-                <form action="{{ route('users.update', $user->id)}}" method="post">
-        {{ method_field('patch') }}
-        <div class="form-group">
-            <label for="">Name:</label>
-        <input class="form-control" value="{{ $user->name }}" disabled>
-        </div>
-        <div class="form-group">
-            <label for="">Email:</label>
-        <input class="form-control" value="{{ $user->email }}" disabled>
-        </div>
-        <div class="form-group">
-                <label for="">User Role</label>
-                <select name="role_id" class="form-control">
-                <option selected>{{$user->role->name}}</option>
-                <option value="1">Admin</option>
-                <option value="2">User</option>
-                </select>
+            @foreach ($users as $user)
+                <div class="col-3">
+                        <form action="{{ route('users.update', $user->id)}}" method="post">
+                            {{ method_field('patch') }}
+                                    <div class="form-group">
+                                        <label for="">Name:</label>
+                                    <input class="form-control" value="{{ $user->name }}" disabled>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="">Email:</label>
+                                    <input class="form-control" value="{{ $user->email }}" disabled>
+                                    </div>
+                                    <div class="form-group">
+                                            <label for="">User Role</label>
+                                            <select name="role_id" class="form-control">
+                                            <option selected>{{$user->role->name}}</option>
+                                            <option value="1">Admin</option>
+                                            <option value="2">User</option>
+                                            </select>
 
-        </div>
+                                    </div>
 
-         <div class="form-group">
-             <label for="">Created</label>
-        <input class="form-control" value="{{ $user->created_at->diffForHumans() }}" disabled>
-        </div>
-        <button class="btn btn-primary" type="submit">Update</button>
-        {{ csrf_field() }}
-    </form>
+                                        <div class="form-group">
+                                            <label for="">Created</label>
+                                        <input class="form-control" value="{{ $user->created_at->diffForHumans() }}" disabled>
+                                        </div>
+                                        <button class="btn btn-primary btn-block mb-1" type="submit">Update</button>
+                                        {{ csrf_field() }}
+                        </form>
+                        <form action="{{ route('users.destroy', $user) }}" method="post">
+                            {{ method_field('delete') }}
+                            <button class="btn btn-danger btn-block" type="submit">Delete</button>
+                            {{ csrf_field() }}
+                        </form>
 
-    <form action="{{ route('users.destroy', $user) }}" method="post">
-     {{ method_field('delete') }}
-        <button class="btn btn-danger" type="submit">Delete</button>
-         {{ csrf_field() }}
-    </form>
-                 @endforeach
-             </div>
+                </div>
+            @endforeach
          </div>
+
 
 
 
