ed53475203945785bbb26667cfe421ea7b7caf08
Chris Lavoie <lavoiect@gmail.com>
2020-06-26 15:48:14 -0600

    remove due date

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -90,10 +90,10 @@ hiynjhknljiyhj kpmokkyhonl6yhlkgpfd;tighkoytyihk]]
 
         <div class="row">
             <div class="col-6">
-                 <div class="form-group">
+              <!--   <div class="form-group">
                     <label for="dueDate">Due Date</label>
                     <input type="date" name="dueDate" class="form-control" value="{{$project->dueDate}}">
-        </div>
+        </div>-->
 
         <div class="form-group">
                     <label for="submittedBy">Submitted By</label>
