13f7748467d94c46a7e22a3780966cabec757add
Chris Lavoie <lavoiect@gmail.com>
2020-07-07 11:22:46 -0600

    remove due this week

--- resources/views/projectFilter.blade.php
+++ resources/views/projectFilter.blade.php
@@ -62,11 +62,7 @@
                         <span class="badge badge-danger">Past Due</span>
 
 
-<!--
-            @elseif ($project->dueDate < $thisweek->addWeek())
-                <span class="badge badge-warning">Due this week</span>
 
-            @else -->
                 <span class="badge badge-success">On Track</span>
             @endif
 
